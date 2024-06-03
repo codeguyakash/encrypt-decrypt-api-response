0073cd048574d037e895a412a83be5e10e814999
Akash <codeguyakash@gmail.com>
2024-06-03 15:05:12 +0530

    add encyption

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require("express");
+const colors = require("colors");
 const https = require("https");
+const CryptoJS = require("crypto-js");
 const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+function encrypt(data, key) {
+  const cipherText = CryptoJS.AES.encrypt(data, key).toString();
+  return cipherText;
+}
+function decrypt(data, key) {
+  try {
+    const bytes = CryptoJS.AES.decrypt(data, key);
+
+    if (bytes.sigBytes > 0) {
+      const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+      return decryptedData;
+    }
+  } catch (error) {
+    throw new Error("Decryption Failed Invalid Key");
+  }
+}
+
+app.get("/encrypt", (req, res) => {
+  const { data, key } = req.body;
+  const encryptData = encrypt(data, key);
+  res.json({ encryptData });
+});
+
+app.get("/decrypt", (req, res) => {
+  const { data, key } = req.body;
+  const encryptData = decrypt(data, key);
+  res.json({ encryptData });
 });
 
 const httpsOptions = {
@@ -16,6 +46,6 @@ const httpsOptions = {
 
 const server = https.createServer(httpsOptions, app);
 
-server.listen(PORT, () => {
-  console.log(`Server is running on https://localhost:${PORT}`);
-});
+server.listen(PORT, () =>
+  console.log(`[Server :: https://localhost:${PORT}]`.black.bgYellow)
+);
